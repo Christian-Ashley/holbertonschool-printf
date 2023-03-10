704b6179681a5b01d3c74477e9762b867db92e97
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-03-10 15:20:20 -0600

    Create c_print.c

---
+++ c_print.c
@@ -0,0 +1,19 @@
+/**
+ * c_print - prints a single character
+ * @list: va_list containinf the char to print as te next element
+ *
+ * Return: number of bytes printed
+ */
+
+int c_print(va_list list)
+{
+        char C = va_list(list, int);
+        int count = 0, retval;
+
+        retval = _putchar(C);
+        if (retval == -1)
+                return (-1);
+        count++;
+
+        return (count);
+}
