4d0f820f927a0d05dc9e9cd17b03a21a2c40e45b
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2023-03-10 15:25:04 -0600

    Create d_print.c

---
+++ d_print.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * countDigits - count how many digits the number is
+ * @num: the number
+ *
+ * Return: the count 
+ */
+
+int countDigits(unsigned int num)
+{
+        int count = 0;
+
+        while (num > 0)
+        {
+                count++;
+                num /= 10;
+        }
+        return (count);
+}
+             /**
+ * print_number - print the number
+ * @n: the number
+ *
+ * Return: return value
+ */
+
+int print_number(unsigned int n)
+{
+        int retval;
+        unsigned int num = n;
+
+        if (num / 10)
+                print_number(num / 10);
+        retval = _putchar('0' + num % 10);
+        if (retval == -1)
+        {
+                return (retval);
+        }
+        return (retval);
+}
+/**
+ * d_print - print decimal numbers, positive and negative
+ * @list: the number to print
+ *
+ * Retun: count
+ */
+int d_print(va_list list)
+{
+        int num = va_list(list, int);
+        int count = 0;
+        int retval;
+        unsigned int unsign;
+
+        if (num < 0)
+        {
+                retvak = _putchar('-');
+                if (retval == -1)
+                        return (1);
+                     else
+                        return (-1);
+        }
+        else
+                unsign = num;
+        retval = print_number(unsign);
+
+        if (reval == 1)
+                count += countDigits(unsign);
+        else count = -1;
+        return (count);
+}
